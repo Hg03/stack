e8823b75a69c595fda25c29a17f550e8afa1b2e9
Hg03 <gehloth03@gmail.com>
2025-08-30 17:48:39 +0000

    updated pipeline

--- src/stack/pipeline.py
+++ src/stack/pipeline.py
@@ -1,12 +1,15 @@
 from stack.configurations.config_validation import MainConfig
+from stack.utils.hopsworks_implementation import get_fs
 from stack.data_pipeline.etl import DataPipeline
+from stack.training_pipeline.trainer import TrainingPipeline
 from omegaconf import OmegaConf, DictConfig
 import hydra
 
 class StackPipeline:
     def __init__(self, config: DictConfig) -> None:
         self.config = OmegaConf.structured(MainConfig(**config))
-        self.data_pipeline = DataPipeline(self.config)
+        self.fs = get_fs()
+        self.data_pipeline = DataPipeline(self.config, self.fs)
     def execute(self):
         print("Starting the data pipeline...")
         data_pipeline_status = self.data_pipeline.run()
